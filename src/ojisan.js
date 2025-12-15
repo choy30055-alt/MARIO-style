09c16cb98a2d85260783c41c8d7331fb20db7eb8
choy30055-alt <choy30055@gmail.com>
2025-12-15 15:15:02 +0900

    Update ojisan.js

--- src/ojisan.js
+++ src/ojisan.js
@@ -33,7 +33,7 @@ class Ojisan {
         this.nokonokoAttack = 0;
         this.fire = 0;
         this.isGoal = false;
-        this.lifePoint = 7;
+        this.lifePoint = 10;
         this.isDead = false;
         this.goalState = 0;
         this.goalTimer = 0;
@@ -751,4 +751,5 @@ class Ojisan {
         }
     }
 }
+
 
